12563fa2215f7f92c6f5727a126f10031526cbb0
Shivam Kumar <shivamkr143@gmail.com>
2018-05-17 18:52:58 +0000

    image can be sorted as per event

--- inter.js
+++ inter.js
@@ -8,3 +8,31 @@ $(document).on('change', '.div-toggle', function() {
 $(document).ready(function(){
     $('.div-toggle').trigger('change');
 });
+
+var cricket1=document.getElementById("cricket");
+var football1=document.getElementById("football");
+var badminton1= document.getElementById("badminton");
+
+function cricket(){
+	cricket1.style.display="block";
+	football1.style.display="none";
+	badminton1.style.display="none";	
+
+}
+function football(){
+	cricket1.style.display="none";
+	football1.style.display="block";
+	badminton1.style.display="none";	
+
+}
+function badminton(){
+	cricket1.style.display="none";
+	football1.style.display="none";
+	badminton1.style.display="block";	
+
+}
+function showall(){
+	cricket1.style.display="block";
+	football1.style.display="block";
+	badminton1.style.display="block";
+}
